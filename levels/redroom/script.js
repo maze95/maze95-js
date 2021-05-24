29c7ab579a2f4804b0d402aedb869db700068754
Agent 11 <techgod18@outlook.com>
2021-05-24 09:24:14 -0400

    Fix overlook in model directory data
    Commited on the github.io, now commiting the changes here.

--- levels/redroom/script.js
+++ levels/redroom/script.js
@@ -1,6 +1,6 @@
 export const redroom_entry = [
     "redroom",
-    "../levels/redroom/model/model.glb",
+    "./levels/redroom/model/model.glb",
     "mus_none",
     [0 /*X*/, 0 /*Y*/, 0 /*Z*/, 0/*yaw*/]
 ]
