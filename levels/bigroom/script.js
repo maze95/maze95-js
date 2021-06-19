f44fc0d50bf4dc6cbc55657e9a261c35ae6eb247
Agent-11 <techgod18@outlook.com>
2021-06-19 13:16:27 -0400

    Title Screen and some code updates

--- levels/bigroom/script.js
+++ levels/bigroom/script.js
@@ -1,8 +1,10 @@
 export const bigroom_entry = [
     "bigroom",
     "./levels/bigroom/model/model.glb",
-    "mus_none",
-    [0 /*X*/, 0 /*Y*/, 0 /*Z*/, 0/*yaw*/],
-    [[0,0,0]],
-    1
+    "mus_bg",
+    [0 /*X*/, 0 /*Y*/, 0 /*Z*/, 0/*yaw*/], //Start pos
+    [[0,0,0]], //Collision box positions (broken)
+    1, //Ambient light intensity
+    null, //Fin (level ender) XYZ pos
+    [0,0.2,-40] //Start logo pos
 ]
--- maze95.js
+++ maze95.js
@@ -11,7 +11,13 @@ import "./lib/settings.js"
 import { faceObj } from "./lib/object_defines.js"
 import { startObj } from "./lib/object_defines.js"
 
+const win95teal = new THREE.Color(0x018281)
+const black = new THREE.Color(0x000000)
+const green = new THREE.MeshBasicMaterial({ color: 0x248000 })
+const red = new THREE.MeshBasicMaterial(0xfc0303)
+
 const scene = new THREE.Scene()
+scene.background = win95teal
 window.spooky = false
 
 function fog(color, near, far) {
@@ -22,30 +28,56 @@ function unfog() {
 }
 const camera = new THREE.PerspectiveCamera(60,width/height)
 
-let green = new THREE.MeshBasicMaterial({color: 0x248000})
-let red = new THREE.MeshBasicMaterial({color: 0xfc0303})
+let titleMus = new Audio("./audio/mus_bg.mp3")
+titleMus.addEventListener('ended', function() { // Thanks @kingjeffrey on stackoverflow for FF loop support!
+  this.currentTime = 0
+  this.play()
+}, false)
+titleMus.play()
 
 let dir = new THREE.Vector3()
 var spd = 0
 window.spd = 0
 window.rotDiv = 17
-let clock = new THREE.Clock
+let clock = new THREE.Clock()
 let gameStarted = false
+let inMaze = true //I am planning to have a pool of maze levels that a random number generator's output determines, this value is for knowing if the player is in a maze level or bigroom due to the model loading function also needing to load in animations which bigroom does not have.
 
-if(!window.ceilingTexName == "ceiling.png") {
+if(window.ceilingTexName !== "ceiling.png") {
   console.log("Custom ceiling texture detected")
 }
-if(!window.floorTexName == "floor.png") {
+if(window.floorTexName !== "floor.png") {
   console.log("Custom floor texture detected")
 }
-if(!window.texturePath == "./textures/") {
+if(window.wallTexName !== "wall.png") {
+  console.log("Custom floor texture detected")
+}
+if(window.texturePath !== "./textures/") {
   console.log("Custom texture path detected")
 }
+
+const wallTex = new THREE.TextureLoader().load(`${texturePath}${window.wallTexName}`, function ( texture ) {
+  texture.wrapS = texture.wrapT = THREE.RepeatWrapping
+  texture.offset.set(0,0)
+  texture.repeat.set(2,2)
+})
+wallTex.magFilter = THREE.NearestFilter
+const wallMat = new THREE.MeshBasicMaterial({map: wallTex})
 
 const playergeo = new THREE.BoxGeometry(3,3,3)
 export const player = new THREE.Mesh(playergeo,green)
 
-var startLis = document.getElementById("start")
+const titleCubeGeo = new THREE.BoxGeometry(15,15,15)
+const titleCube = new THREE.Mesh(titleCubeGeo, wallMat)
+scene.add(titleCube)
+titleCube.position.z = -30
+titleCube.position.x = 7
+titleCube.rotation.y = 0.45
+
+var classicstartLis = document.getElementById("classicstart")
+var storystartLis = document.getElementById("storystart")
+storystartLis.disabled = true
+console.log("Story Mode unimplemented")
 
 scene.add(player)
 scene.add(camera)
@@ -53,7 +85,7 @@ scene.add(camera)
 const ceilingTex = new THREE.TextureLoader().load(`${texturePath}${window.ceilingTexName}`, function ( texture ) {
   texture.wrapS = texture.wrapT = THREE.RepeatWrapping
   texture.offset.set(0,0)
-  texture.repeat.set(40,30)
+  texture.repeat.set(80,70)
 })
 ceilingTex.magFilter = THREE.NearestFilter
 const ceilingMat = new THREE.MeshBasicMaterial({map: ceilingTex})
@@ -61,7 +93,7 @@ const ceilingMat = new THREE.MeshBasicMaterial({map: ceilingTex})
 const floorTex = new THREE.TextureLoader().load(`${texturePath}${window.floorTexName}`, function ( texture ) {
   texture.wrapS = texture.wrapT = THREE.RepeatWrapping
   texture.offset.set(0,0)
-  texture.repeat.set(20,20)
+  texture.repeat.set(25,25)
 })
 floorTex.magFilter = THREE.NearestFilter
 const floorMat = new THREE.MeshBasicMaterial({map: floorTex})
@@ -75,21 +107,23 @@ const ceiling = new THREE.Mesh(
   ceilingMat
 )
 
-if(!SelectedLVL("lvlMus") == "mus_none") {
-  let mus = new Audio('../audio/' + SelectedLVL("lvlMus") + '.mp3')
-  mus.addEventListener('ended', function() { // Thanks @kingjeffrey on stackoverflow for FF loop support!
-    this.currentTime = 0
-    this.play()
-  }, false)
-  mus.play()
+function playMusic() {
+  if(SelectedLVL("lvlMus") !== "mus_none") {
+    let mus = new Audio('./audio/' + SelectedLVL("lvlMus") + '.mp3')
+    mus.addEventListener('ended', function() { // Thanks @kingjeffrey on stackoverflow for FF loop support!
+      this.currentTime = 0
+      this.play()
+    }, false)
+    mus.play()
+  }
 }
 
 const renderer = new THREE.WebGL1Renderer({
-  canvas: document.querySelector('#bg'),
+  canvas: document.querySelector('#game'),
 })
 
 renderer.setPixelRatio(window.devicePixelRatio)
-if(widescreen == false) {
+if(!widescreen) {
   renderer.setSize(width, height)
 }
 else {
@@ -104,8 +138,8 @@ const widesizes = {
 
 //Init widescreen functionality
 if(widescreen) {
-  width = innerWidth
-  height = innerHeight
+  width = window.innerWidth
+  height = window.innerHeight
 }
 
 var mixer
@@ -130,28 +164,46 @@ window.addEventListener('resize', () =>
   }
 })
 
+function clearCanvas() {
+  document.getElementById("title").remove()
+  classicstartLis.remove()
+  storystartLis.remove()
+  titleMus.pause()
+}
+
 function loadModel() {
   var loader = new GLTFLoader();
   loader.load(SelectedLVL("lvlDir"), function (gltf) {
-  mixer = new THREE.AnimationMixer(gltf.scene)
+  if(SelectedLVL("lvlName") !== "maze") {
+    inMaze = false
+    scene.remove(floor)
+    scene.remove(ceiling)
+  }
+  if(inMaze) {
+    mixer = new THREE.AnimationMixer(gltf.scene)
 
-  action = mixer.clipAction(gltf.animations[ 0 ])
-  action.setLoop(THREE.LoopOnce)
-  action.clampWhenFinished = true
-  action.enable = true
-  action.play()
-  action.play().reset()
-  window.spd = 0.9
-  spd = window.spd
+    action = mixer.clipAction(gltf.animations[ 0 ])
+    action.setLoop(THREE.LoopOnce)
+    action.clampWhenFinished = true
+    action.enable = true
+    action.play()
+    action.play().reset()
+  }
+  else {
+    console.log("Level not a maze level; skipping animation initalization")
+  }
 
   scene.add(gltf.scene)
   gltf.scene.position.y = 8
   })
+  window.spd = 0.9
+  spd = window.spd
 }
 
-startLis.onclick = function startGame() {
-  document.getElementById("start").disabled = true
-
+classicstartLis.onclick = function startGame() {
+  document.getElementById("classicstart").disabled = true
+  scene.background = black
+  clearCanvas()
   loadModel()
   loadMap()
 }
@@ -162,15 +214,10 @@ resetButton.onclick = function resetGame() {
   location.reload()
 }
 
-function sleep(milliseconds) {
-  const date = Date.now();
-  let currentDate = null;
-  do {
-    currentDate = Date.now();
-  } while (currentDate - date < milliseconds);
-}
+let amb
 
 function loadMap() {
+  scene.remove(titleCube)
   floor.position.set(0,-18,-215)
   ceiling.position.set(0,12,-215)
   player.position.z = -23
@@ -179,10 +226,10 @@ function loadMap() {
   scene.add(faceObj)
   scene.add(startObj)
 
-  const amb = new THREE.AmbientLight(0xffffff, SelectedLVL("ambLightIntensity"))
+  amb = new THREE.AmbientLight(0xffffff, SelectedLVL("ambLightIntensity"))
   scene.add(amb)
   gameStarted = true
-  
+  playMusic()
 }
 
 function redraw() {
@@ -196,8 +243,13 @@ function redraw() {
   faceObj.rotation.y = player.rotation.y
   startObj.rotation.y = player.rotation.y
 
-  if(spooky) {
+  if(window.spooky && !gameStarted) {
+    console.warn("Cannot enable spooky mode yet, map not loaded.") //Any help on how not to newly warn every single framew would be appreciated
+  }
+
+  if(window.spooky && gameStarted) {
     fog(0x0000, 5, 50)
+    amb.color.setHex(0x707070)
   }
   else {
     unfog()
@@ -248,4 +300,3 @@ kd.L.down(
 //Execute keydrown tick and run redraw
 kd.run(function(){kd.tick()})
 redraw()
-//F in the chat for luphoria aswell, his Discord account got disabled because he entered a trap server afaik, Tragic.
