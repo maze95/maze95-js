3d9c0d32a5c4905016418f9e4eeff72cbd441a4d
Agent-11 / Agent X <44549182+Agent-11@users.noreply.github.com>
2021-12-05 18:37:10 -0500

    Add a utils file to help out

---
+++ game/utils.js
@@ -0,0 +1,21 @@
+import * as Player from "./player_controller.js"
+import { startObj } from "./object_defines.js"
+
+let interval = 1
+export const moveOut = () => {
+  if (Player.checkCollided()) {
+    Player.pObj.position.x += interval
+    // if still not out of wall
+    if (Player.checkCollided()) {
+      interval += 2
+      moveOut()
+    } else {
+        startObj.position.x = interval
+    }
+  }
+}
+moveOut()
+
+export const isInRange = (val, low, high) => {
+    if (val < high && val > low) return true
+}
