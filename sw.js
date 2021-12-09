c7faa01a278c3fc01b5dac18530c58038fa5ee27
Agent-11 / Agent X <44549182+Agent-11@users.noreply.github.com>
2021-12-09 16:29:49 -0500

    Update service worker

--- sw.js
+++ sw.js
@@ -1,74 +1,93 @@
-/*
- Copyright 2016 Google Inc. All Rights Reserved.
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
- http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- */
+var VERSION = 'v1';
 
-// Names of the two caches used in this version of the service worker.
-// Change to v2, etc. when you update any of the local resources, which will
-// in turn trigger the install event again.
-const PRECACHE = 'precache-v1';
-const RUNTIME = 'runtime';
+var cacheFirstFiles = [
+  // add paths and URLs to pull from cache first if it has been loaded before. Else fetch from network.
+  // if loading from cache, fetch from network in the background to update the resource. Examples:
+  "textures/button.png",
+  "textures/ceiling.png",
+  "textures/fin.png",
+  "textures/finbig.png",
+  "textures/floor.png",
+  "textures/start.png",
+  "textures/wall.png",
 
-// A list of local resources we always want to be cached.
-const PRECACHE_URLS = [
-  'index.html',
-  './', // Alias for index.html
-  'index.css',
-  'maze95.js'
+  "index.css",
+
+  "canvas-only.html",
+
+  "sw.js",
+  "game/generation.js",
+  "game/keydrown.min.js",
+  "game/model_loader.js",
+  "game/object_defines.js",
+  "game/player_controller.js",
+  "game/surface.js",
+  "game/three.module.js",
+  "game/utils.js",
+
 ];
 
-// The install handler takes care of precaching the resources we always need.
+var networkFirstFiles = [
+  // add paths and URLs to pull from network first. Else fall back to cache if offline
+  "maze95.js",
+  "index.html"
+];
+
+// Below is the service worker code.
+
+var cacheFiles = cacheFirstFiles.concat(networkFirstFiles);
+
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(PRECACHE)
-      .then(cache => cache.addAll(PRECACHE_URLS))
-      .then(self.skipWaiting())
+    caches.open(VERSION).then(cache => {
+      return cache.addAll(cacheFiles);
+    })
   );
 });
 
-// The activate handler takes care of cleaning up old caches.
-self.addEventListener('activate', event => {
-  const currentCaches = [PRECACHE, RUNTIME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-    }).then(cachesToDelete => {
-      return Promise.all(cachesToDelete.map(cacheToDelete => {
-        return caches.delete(cacheToDelete);
-      }));
-    }).then(() => self.clients.claim())
-  );
-});
-
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
 self.addEventListener('fetch', event => {
-  // Skip cross-origin requests, like those for Google Analytics.
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
-      })
-    );
+  if (event.request.method !== 'GET') { return; }
+  if (networkFirstFiles.indexOf(event.request.url) !== -1) {
+    event.respondWith(networkElseCache(event));
+  } else if (cacheFirstFiles.indexOf(event.request.url) !== -1) {
+    event.respondWith(cacheElseNetwork(event));
+  } else {
+    event.respondWith(fetch(event.request));
   }
 });
+
+// If cache else network.
+// For images and assets that are not critical to be fully up-to-date.
+// developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook/
+// #cache-falling-back-to-network
+function cacheElseNetwork (event) {
+  return caches.match(event.request).then(response => {
+    function fetchAndCache () {
+       return fetch(event.request).then(response => {
+        // Update cache.
+        caches.open(VERSION).then(cache => cache.put(event.request, response.clone()));
+        return response;
+      });
+    }
+
+    // If not exist in cache, fetch.
+    if (!response) { return fetchAndCache(); }
+
+    // If exists in cache, return from cache while updating cache in background.
+    fetchAndCache();
+    return response;
+  });
+}
+
+// If network else cache.
+// For assets we prefer to be up-to-date (i.e., JavaScript file).
+function networkElseCache (event) {
+  return caches.match(event.request).then(match => {
+    if (!match) { return fetch(event.request); }
+    return fetch(event.request).then(response => {
+      // Update cache.
+      caches.open(VERSION).then(cache => cache.put(event.request, response.clone()));
+      return response;
+    }) || response;
+  });
+}
