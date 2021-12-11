6a93c86e0763970561c192430f5c3600a3c9b6ff
Agent-11 / Agent X <44549182+Agent-11@users.noreply.github.com>
2021-12-10 21:59:49 -0500

    Fully work offline, add WIP mobile version
    Add W.I.P mobile page with DPad
    Improve manifest.json
    Improve SW so Maze 95 JS now fully works offline

--- maze95.js
+++ maze95.js
@@ -1,18 +1,20 @@
 import * as THREE from './game/three.module.js' // Maze 95 JS, now in 3D!
-import * as MazeGen from "./game/generation.js"
-import * as Utils from "./game/utils.js"
+import * as MazeGen from "./game/generation.js" // code for generating maze
+import * as Utils from "./game/utils.js" // various tools to help out
 import * as Player from "./game/player_controller.js"
-import "./game/keydrown.min.js"
+import "./game/keydrown.min.js" // input library
 
+// ceiling and floor material to apply to floor and ceiling object input
 import { ceilingMat, floorMat } from "./textures/textures.js"
 
 import { startObj } from "./game/object_defines.js" // object
 
+// viewport properties
 window.widescreen = false
 let width = 640
 let height = 480
 
-export const renderer = new THREE.WebGLRenderer({
+let renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#game'),
   antialias: false
 })
@@ -24,26 +26,14 @@ if (widescreen) {
 }
 const camera = new THREE.PerspectiveCamera(60,width/height)
 
-if (window.location.href.includes("full")) {
-  window.widescreen = true
-  // update sizes
-  width = window.innerWidth
-  height = window.innerHeight
-  // update camera
-  camera.aspect = width / height
-  camera.updateProjectionMatrix()
-  // update renderer
-  renderer.setSize(width, height)
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-}
-
-//Dynamic scaling for widescreen
+let div = 1
+// dynamic scaling for widescreen
 window.addEventListener('resize', () =>
 {
   if (window.widescreen) {
     // update sizes
     width = window.innerWidth
-    height = window.innerHeight
+    height = window.innerHeight / div
     // update camera
     camera.aspect = width / height
     camera.updateProjectionMatrix()
@@ -54,7 +44,48 @@ window.addEventListener('resize', () =>
   }
 })
 
-if ('ontouchstart' in document.documentElement && !window.location.href.includes("full")) window.location.href = "./full.html" // if mobile
+if ('ontouchstart' in document.documentElement && !window.location.href.includes("mobile")) window.location.href = "./mobile.html"
+if (window.location.href.includes("mobile")) {
+  div = 2
+  window.widescreen = true
+  // update sizes
+  width = window.innerWidth
+  height = window.innerHeight / div
+  // update camera
+  camera.aspect = width / height
+  camera.updateProjectionMatrix()
+  // update renderer
+  renderer.setSize(width, height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+  document.getElementById("upBtn").addEventListener('touchstart', () => {
+    window.playerInput.up = true
+  })
+  document.getElementById("upBtn").addEventListener('touchend', () => {
+    window.playerInput.up = false
+  })
+
+  document.getElementById("leftBtn").addEventListener('touchstart', () => {
+    window.playerInput.left = true
+  })
+  document.getElementById("leftBtn").addEventListener('touchend', () => {
+    window.playerInput.left = false
+  })
+
+  document.getElementById("backBtn").addEventListener('touchstart', () => {
+    window.playerInput.down = true
+  })
+  document.getElementById("backBtn").addEventListener('touchend', () => {
+    window.playerInput.down = false
+  })
+
+  document.getElementById("rightBtn").addEventListener('touchstart', () => {
+    window.playerInput.right = true
+  })
+  document.getElementById("rightBtn").addEventListener('touchend', () => {
+    window.playerInput.right = false
+  })
+}
 
 export const scene = new THREE.Scene()
 scene.add(Player.pObj)
@@ -76,7 +107,6 @@ const floorGeo = new THREE.BoxGeometry(1000,1,1000)
 const floor = new THREE.Mesh(floorGeo, floorMat)
 floor.position.y = -13
 scene.add(floor)
-
 const ceilingGeo = new THREE.BoxGeometry(1000,1,1000)
 const ceiling = new THREE.Mesh(ceilingGeo, ceilingMat)
 ceiling.position.y = 9
@@ -87,6 +117,13 @@ scene.add(ceiling)
 
 Utils.moveOut()
 
+window.playerInput = {
+  up: false,
+  down: false,
+  left: false,
+  right: false
+}
+
 function update() {
   requestAnimationFrame(update)
   camera.position.x = Player.pObj.position.x
@@ -96,13 +133,23 @@ function update() {
 
   startObj.rotation.y = Player.pObj.rotation.y
 
+  if (window.playerInput.up) { Player.playerAction("move", -window.spd) }
+  if (window.playerInput.down) { Player.playerAction("move", window.spd) }
+  if (window.playerInput.left) { Player.playerAction("rotate", window.spd / window.rotDiv) }
+  if (window.playerInput.right) { Player.playerAction("rotate", -window.spd / window.rotDiv) }
+
   renderer.render(scene, camera)
 }
 
-kd.W.down(()=> {Player.playerAction("move", -window.spd)})
-kd.S.down(()=> {Player.playerAction("move", window.spd)})
-kd.A.down(()=> {Player.playerAction("rotate", window.spd / window.rotDiv)})
-kd.D.down(()=> {Player.playerAction("rotate", -window.spd / window.rotDiv)})
+kd.W.down(()=> {window.playerInput.up    = true})
+kd.S.down(()=> {window.playerInput.down  = true})
+kd.A.down(()=> {window.playerInput.left  = true})
+kd.D.down(()=> {window.playerInput.right = true})
+
+kd.W.up(()=> {window.playerInput.up    = false})
+kd.S.up(()=> {window.playerInput.down  = false})
+kd.A.up(()=> {window.playerInput.left  = false})
+kd.D.up(()=> {window.playerInput.right = false})
 kd.run(function(){kd.tick()})
 
 update()
